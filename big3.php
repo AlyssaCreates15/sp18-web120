7f085328ddf0ce55a9a8bc81fa93d9347243e917
AlyssaCreates15 <39714968+AlyssaCreates15@users.noreply.github.com>
2018-08-21 20:50:48 -0700

    Updated

---
+++ big3.php
@@ -0,0 +1,22 @@
+<?php include 'includes/header.php'?>
+
+<main> <h1>BIG Project Research: Responsive Website vs Mobile App</h1>
+
+	The importance of accessibility spans from lawsuit liability to providing the best usability for your target audience and giving your client the best product you possibly can.  Being a web designer is much larger than making design and incorporating the clients logos and vision into a tangible place for their users to interact with them. A user’s first impression of a company or person may be through the web, and it is the first chance to showcase your brand and interest users in your product. User experience, user interface, and usability are integral pieces of the the responsibility the designer must take into consideration. 
+
+	 In comparing a mobile app versus a responsive website, there are a few things to consider.  There are pros and cons to both, and it is best to focus on the specific needs of the client and potential users. The cost of a website is less expensive, easier to update and support, and available to all users (Smartstack). On the other hand, mobile apps typically take specific cell phone user experience into mind, more secure than websites, can work offline, and utilize the cell phone components (Thinkmobiles).  Determining the client’s specific needs and researching the user pool will help to determine which is the best choice.  An example would be developing an app for a cell phone company. If your target audience is cell phone users, in the 15-45 age bracket, and user information security is an issue- utilizing a web app makes more sense.  Otherwise, if you are an art dealer, wanting to showcase beautiful pieces of photography for potential buyers- having a responsive website is more valuable.  The content you will be showcasing needs to maintain quality and doesn’t make sense to be on a mobile app. 
+
+	 Each client will have specific needs and will need a fresh approach when determining which is best for each client.  The takeaway for web designers is the importance of user experience and giving your client the best product for their needs. 
+
+
+</main>
+
+<aside>
+ References:
+
+1. “Responsive Website vs MObile app: choose one or both”,  Thinkmobiles, 20 July 2018,  https://thinkmobiles.com/blog/responsive-website-vs-mobile-app/.
+2. “The Pros and Cons of Mobile Apps vs Responsive Web Design”, Smashstack, https://www.smashstack.com/articles/the-pros-and-cons-of-mobile-apps-vs-responsive-web-design/.
+
+</aside>
+
+<?php include 'includes/footer.php'?>
